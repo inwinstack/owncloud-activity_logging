caa8a8355ed6175b6b514ba22d6d257ffaa61600
Julius Chen <julius.j@inwinstack.com>
2016-01-15 18:10:57 +0800

    Add Share File Log Record

--- lib/userhooks.php
+++ lib/userhooks.php
@@ -7,7 +7,6 @@ use OCP\User;
 use OC\Files;
 
 class UserHooks {
-
     private $userManager;
     private $UserFolder;
 
@@ -16,30 +15,42 @@ class UserHooks {
         $this->UserFolder = $userFolder;
     }
 
+    public static function share($params) {
+        if ( ($params['shareType'] === \OCP\Share::SHARE_TYPE_USER) || ($params['shareType'] === \OCP\Share::SHARE_TYPE_GROUP) ) {
+            $params['shareType'] === \OCP\Share::SHARE_TYPE_USER ? $shareType = " with User: " : $shareType = " with Group: ";
+            Util::writeLog('activity',"user:" . $params['uidOwner'] . " action:share " . $params['fileTarget'] . $shareType . $params['shareWith'], Util::INFO);
+        }
+        else {
+            Util::writeLog('activity',"user:" . $params['uidOwner'] . " action:share " . $params['fileTarget'] . ' with link', Util::INFO);
+        }
+    }
+
     public function register() {
         $loginRecord = function($user) {
-            \OCP\Util::writeLog('core',"user:" . \OCP\User::getDisplayName() . " action:login success", \OCP\Util::INFO);
+            Util::writeLog('core',"user:" . User::getDisplayName() . " action:login success", Util::INFO);
         };
 
         $logoutRecord = function() {
-            \OCP\Util::writeLog('core',"user:" . \OCP\User::getDisplayName() . " action:logout success", \OCP\Util::INFO);
+            Util::writeLog('core',"user:" . User::getDisplayName() . " action:logout success", Util::INFO);
         };
 
         $createRecord = function($node) {
-            \OCP\Util::writeLog('activity',"user:" . \OCP\User::getDisplayName() . " action:cretes " . $node->getName() . " sucess", \OCP\Util::INFO);
+            Util::writeLog('activity',"user:" . User::getDisplayName() . " action:cretes " . $node->getName() . " sucess", Util::INFO);
         };
 
         $deleteRecord = function($node) {
-            \OCP\Util::writeLog('activity',"user:" . \OCP\User::getDisplayName() . " action:deletes " . $node->getName() . " sucess", \OCP\Util::INFO);
+            Util::writeLog('activity',"user:" . User::getDisplayName() . " action:deletes " . $node->getName() . " sucess", Util::INFO);
         };
 
         $renameRecord = function($node) {
-            \OCP\Util::writeLog('activity',"user:" . \OCP\User::getDisplayName() . " action:renames " . $node->getName() . " sucess", \OCP\Util::INFO);
+            Util::writeLog('activity',"user:" . User::getDisplayName() . " action:renames " . $node->getName() . " sucess", Util::INFO);
         };
 
         $touchRecord = function($node) {
-            \OCP\Util::writeLog('activity',"user:" . \OCP\User::getDisplayName() . " action:touches " . $node->getName() . " sucess", \OCP\Util::INFO);
+            Util::writeLog('activity',"user:" . User::getDisplayName() . " action:touches " . $node->getName() . " sucess", Util::INFO);
         };
+
+        Util::connectHook('OCP\Share', 'post_shared', 'OCA\Activity_Logging\UserHooks', 'share');
 
         $this->userManager->listen('\OC\User', 'postLogin', $loginRecord);
         $this->userManager->listen('\OC\User', 'logout', $logoutRecord);
