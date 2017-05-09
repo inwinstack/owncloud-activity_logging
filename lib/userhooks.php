4b1646510c405e059d89422b9673895f88d51cd4
Duncan Chiang <duncan.c@inwinstack.com>
2017-05-09 15:52:42 +0800

    [Feature] Add region/schoolCode/agent info in log.

--- lib/userhooks.php
+++ lib/userhooks.php
@@ -15,47 +15,196 @@ class UserHooks {
         $this->userManager = $userManager;
         $this->UserFolder = $userFolder;
     }
-
+    
+    public static function formatLogMessage($user,$actionMsg){
+        $UserRole = \OC::$server->getConfig()->getUserValue(\OC_User::getUser(), "settings", "role","undefined");
+        $regionData = self::formatRegionData();
+        $UserRegion = $regionData[0];
+        $UserSchoolCode = $regionData[1];
+        
+        return "user:" . $user.
+                " role:" . $UserRole .
+                " region:" . $UserRegion .
+                " schoolCode:" . $UserSchoolCode .
+                " agent:" . self::checkAgent() .
+                " action:" . $actionMsg;
+    }
+    
+    
     public static function share($params) {
+        
+        $regionData = self::formatRegionData();
+        $UserRegion = $regionData[0];
+        $UserSchoolCode = $regionData[1];
+        
         if ( ($params['shareType'] === \OCP\Share::SHARE_TYPE_USER) || ($params['shareType'] === \OCP\Share::SHARE_TYPE_GROUP) ) {
             $params['shareType'] === \OCP\Share::SHARE_TYPE_USER ? $shareType = " with User: " : $shareType = " with Group: ";
-            Util::writeLog('activity',"user:" . $params['uidOwner'] . " action:share " . $params['fileTarget'] . $shareType . $params['shareWith'], Util::INFO);
+            
+            Util::writeLog('core',
+                            self::formatLogMessage($params['uidOwner'], 
+                                    "share " . $params['fileTarget'] . $shareType . $params['shareWith']), 
+                            Util::INFO);
+
         }
         else {
-            Util::writeLog('activity',"user:" . $params['uidOwner'] . " action:share " . $params['fileTarget'] . ' with link', Util::INFO);
+            Util::writeLog('core',
+                            self::formatLogMessage($params['uidOwner'], 
+                                    "share " . $params['fileTarget'] . ' with link'),
+                            Util::INFO);
+
         }
     }
-
+    
+    public static function getOS($userAgent) {
+        $os_platform    =   "undefined";
+        
+        $os_array       =   array(
+                '/windows/i'           => 'Windows',
+                '/windows nt 10/i'     =>  'Windows',
+                '/windows nt 6.3/i'     =>  'Windows',
+                '/windows nt 6.2/i'     =>  'Windows',
+                '/windows nt 6.1/i'     =>  'Windows',
+                '/windows nt 6.0/i'     =>  'Windows',
+                '/windows nt 5.2/i'     =>  'Windows',
+                '/windows nt 5.1/i'     =>  'Windows',
+                '/windows xp/i'         =>  'Windows',
+                '/windows nt 5.0/i'     =>  'Windows',
+                '/windows me/i'         =>  'Windows',
+                '/win98/i'              =>  'Windows',
+                '/win95/i'              =>  'Windows',
+                '/win16/i'              =>  'Windows',
+                '/macintosh|mac os x/i' =>  'MacOS',
+                '/mac_powerpc/i'        =>  'MacOS',
+                '/linux/i'              =>  'Linux',
+                '/ubuntu/i'             =>  'Ubuntu',
+                '/iphone/i'             =>  'iOS',
+                '/ipod/i'               =>  'iOS',
+                '/ipad/i'               =>  'iOS',
+                '/android/i'            =>  'Android',
+                '/blackberry/i'         =>  'BlackBerry',
+                '/webos/i'              =>  'Mobile'
+        );
+        
+        foreach ($os_array as $regex => $value) {
+        
+            if (preg_match($regex, $userAgent)) {
+                $os_platform    =   $value;
+            }
+        
+        }
+        
+        return $os_platform;
+    }
+    
+    public static function getBrowser($userAgent) {
+        $browser        =   "undefined";
+        $browser_array  =   array(
+                '/msie/i'       =>  'IE',
+                '/firefox/i'    =>  'Firefox',
+                '/safari/i'     =>  'Safari',
+                '/chrome/i'     =>  'Chrome',
+                '/edge/i'       =>  'Edge',
+                '/opera/i'      =>  'Opera',
+                '/netscape/i'   =>  'Netscape',
+                '/maxthon/i'    =>  'Maxthon',
+                '/konqueror/i'  =>  'Konqueror',
+                '/mobile/i'     =>  'Handheld_Browser',
+                '/MOE_Storage/i'=>  'Desktop',
+        );
+        
+        foreach ($browser_array as $regex => $value) {
+        
+            if (preg_match($regex, $userAgent)) {
+                $browser    =   $value;
+            }
+        
+        }
+        
+        return $browser;
+    }
+    
+    public static function checkAgent(){
+        if(isset($_SERVER['HTTP_USER_AGENT']) and !empty($_SERVER['HTTP_USER_AGENT'])){
+            $agent = $_SERVER['HTTP_USER_AGENT'];
+            return self::getBrowser($agent). '_' .self::getOS($agent);
+        }
+        return 'undefined_undefined';
+    }
+    
+    public static function formatRegionData(){
+        $regionData = \OC::$server->getConfig()->getUserValue(\OC_User::getUser(), "settings", "regionData",false);
+        //$regionData = ['region':1, 'schoolCode':1234]
+        if ($regionData){
+            $regionData = json_decode($regionData,true);
+            $UserRegion = $regionData['region'];
+            $UserSchoolCode = $regionData['schoolCode'];
+        }
+        else{
+            $UserRegion = "undefined";
+            $UserSchoolCode = "undefined";
+        }
+        return [$UserRegion,$UserSchoolCode];
+    }
+    
     public function register() {
         $loginRecord = function($user) {
-            $UserRole = \OC::$server->getConfig()->getUserValue(\OC_User::getUser(), "settings", "role","undefined");
-            Util::writeLog('core',"user:" . User::getDisplayName() . " role:" . $UserRole . " action:login success", Util::INFO);
+
+            Util::writeLog('core',
+                            self::formatLogMessage(User::getDisplayName(), 
+                                                    "login success"), 
+                            Util::INFO);
+
         };
 
         $logoutRecord = function() {
-            $UserRole = \OC::$server->getConfig()->getUserValue(\OC_User::getUser(), "settings", "role","undefined");
-            Util::writeLog('core',"user:" . User::getDisplayName() . " role:" . $UserRole . " action:logout success", Util::INFO);
+            
+            Util::writeLog('core',
+                            self::formatLogMessage(User::getDisplayName(), 
+                                                    "logout success"), 
+                            Util::INFO);
+            
         };
 
         $createRecord = function($node) {
-            $UserRole = \OC::$server->getConfig()->getUserValue(\OC_User::getUser(), "settings", "role","undefined");
-            Util::writeLog('activity',"user:" . User::getDisplayName() . " role:" . $UserRole . " action:creates " . $node->getName() . " success", Util::INFO);
+   
+            Util::writeLog('core',
+                            self::formatLogMessage(User::getDisplayName(), 
+                                                    "creates " . $node->getName() ." success"), 
+                            Util::INFO);
+            
         };
 
         $deleteRecord = function($node) {
-            $UserRole = \OC::$server->getConfig()->getUserValue(\OC_User::getUser(), "settings", "role","undefined");
-            Util::writeLog('activity',"user:" . User::getDisplayName() . " role:" . $UserRole . " action:deletes " . $node->getName() . " success", Util::INFO);
+
+            Util::writeLog('core',
+                            self::formatLogMessage(User::getDisplayName(), 
+                                                    "deletes " . $node->getName() . " success"),
+                            Util::INFO);
+            
         };
 
         $renameRecord = function($node) {
-            $UserRole = \OC::$server->getConfig()->getUserValue(\OC_User::getUser(), "settings", "role","undefined");
-            Util::writeLog('activity',"user:" . User::getDisplayName() . " role:" . $UserRole . " action:renames " . $node->getName() . " success", Util::INFO);
+
+            Util::writeLog('core',
+                            self::formatLogMessage(User::getDisplayName(), 
+                                                    "renames " . $node->getName() . " success"),
+                            Util::INFO);
+            
         };
 
-        $touchRecord = function($node) {
-            $UserRole = \OC::$server->getConfig()->getUserValue(\OC_User::getUser(), "settings", "role","undefined");
-            Util::writeLog('activity',"user:" . User::getDisplayName() . " role:" . $UserRole . " action:touches " . $node->getName() . " success", Util::INFO);
-        };
+//         $touchRecord = function($node) {
+//             $UserRole = \OC::$server->getConfig()->getUserValue(\OC_User::getUser(), "settings", "role","undefined");
+            
+//             $regionData = self::formatRegionData();
+//             $UserRegion = $regionData[0];
+//             $UserSchoolCode = $regionData[1];
+            
+//             Util::writeLog('activity',"user:" . User::getDisplayName() . 
+//                                       " role:" . $UserRole .
+//                                       " region:" . $UserRegion .
+//                                       " schoolCode:" . $UserSchoolCode .
+//                                       " action:touches " . $node->getName() . " success", Util::INFO);
+//         };
 
         Util::connectHook('OCP\Share', 'post_shared', 'OCA\Activity_Logging\UserHooks', 'share');
 
@@ -68,4 +217,5 @@ class UserHooks {
         $this->UserFolder->listen('\OC\Files', 'postRename', $renameRecord);
         }
 }
+
 
